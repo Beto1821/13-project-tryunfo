90155d6e80ad0c0d1dc939950f3663a271d5df4e
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-05-13 17:43:35 -0300

    req03

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   state = {
@@ -48,6 +49,7 @@ class App extends React.Component {
             hasTrunfo={ hasTrunfo }
             isSaveButtonDisabled={ isSaveButtonDisabled }
           />
+          <Card />
         </form>
       </div>
     );
