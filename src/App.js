6d2fcf98908fe27c5a6ee7ca0f2d72270e31f3e0
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-05-14 21:27:38 -0300

    req08

--- src/App.js
+++ src/App.js
@@ -115,6 +115,7 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      cards,
     } = this.state;
     return (
       <div className="pai">
@@ -143,6 +144,23 @@ class App extends React.Component {
           cardTrunfo={ cardTrunfo }
           hasTrunfo={ hasTrunfo }
         />
+        <div>
+          { cards.map((card) => (
+            <div key={ card.cardName }>
+              <Card
+                cardName={ card.cardName }
+                cardDescription={ card.cardDescription }
+                cardAttr1={ card.cardAttr1 }
+                cardAttr2={ card.cardAttr2 }
+                cardAttr3={ card.cardAttr3 }
+                cardImage={ card.cardImage }
+                cardRare={ card.cardRare }
+                cardTrunfo={ card.cardTrunfo }
+                hasTrunfo={ card.hasTrunfo }
+              />
+            </div>))}
+          ;
+        </div>
       </div>
     );
   }
